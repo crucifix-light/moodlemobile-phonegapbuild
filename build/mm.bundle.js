db37cc9e07a9f8da1ff8e2ebe70c5afe572ff7c0
crucifix-light <crucifix.sacrifice.light@gmail.com>
2018-06-25 15:16:53 +0800

    Add files via upload
    Changes
    - change dev URL to prod URL

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -11858,7 +11858,7 @@ angular.module('mm.core.login', [])
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl',
         onEnter: function($state) {
-            $state.go('mm_login.credentials', {siteurl: 'http://10.100.0.35/lms'});
+            $state.go('mm_login.credentials', {siteurl: 'https://ilearn.bsp.gov.ph/lms'});
         }
     })
     .state('mm_login.credentials', {
